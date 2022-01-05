c5d38cb25a171738c16b95ab2beae97fec72fd88
Shreyaan Seth <Shreyaans41@gmail.com>
2022-01-05 22:21:58 +0530

    without local storage

---
+++ app.js
@@ -0,0 +1,55 @@
+//selectors
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+
+
+//event listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck)
+
+//functions
+function addTodo(event) {
+    // prevent from from submitting 
+    event.preventDefault()
+    // todo div
+    const todoDiv = document.createElement('div')
+    todoDiv.classList.add('todo')
+    // todo li
+    const newTodo = document.createElement('li')
+    newTodo.innerText = todoInput.value
+    newTodo.classList.add('todo-item')
+    todoDiv.appendChild(newTodo)
+    // checkmark button
+    const completedButton = document.createElement("button")
+    completedButton.innerHTML = `<i class="fas fa-check" ></i>`
+    completedButton.classList.add('complete-btn')
+    todoDiv.appendChild(completedButton)
+    // trash button
+    const trashButton = document.createElement("button")
+    trashButton.innerHTML = `<i class="fas fa-trash" ></i>`
+    trashButton.classList.add('trash-btn')
+    todoDiv.appendChild(trashButton)
+
+
+    todoList.appendChild(todoDiv)
+
+    todoInput.value = ''
+}
+
+function deleteCheck(e) {
+    const item = e.target
+    if (item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement
+        todo.classList.toggle('fall')
+        todo.addEventListener('transitionend',function(){
+            todo.remove()
+        })
+    }
+
+    if (item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement
+        todo.classList.toggle('completed')
+    }
+
+}
